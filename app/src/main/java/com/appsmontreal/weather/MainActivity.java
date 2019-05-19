370f3a408875584c8c810ddde95a61fe3b5ce8a7
Marlon <escobar.marlon@gmail.com>
2019-05-19 15:17:12 -0400

    endApplication method implementation

--- app/src/main/java/com/appsmontreal/weather/MainActivity.java
+++ app/src/main/java/com/appsmontreal/weather/MainActivity.java
@@ -2,12 +2,30 @@ package com.appsmontreal.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button searchButton;
+    Button exitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize() {
+        searchButton = findViewById(R.id.buttonSearch);
+        exitButton = findViewById(R.id.buttonExit);
+    }
+
+    public void endApplication(){
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
