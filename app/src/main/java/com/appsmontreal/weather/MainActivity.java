19e353827d0a170616f92b7de3b0a61b971bf655
Marlon <escobar.marlon@gmail.com>
2019-05-20 21:32:29 -0400

    DownloadTask class implemented

--- app/src/main/java/com/appsmontreal/weather/MainActivity.java
+++ app/src/main/java/com/appsmontreal/weather/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
